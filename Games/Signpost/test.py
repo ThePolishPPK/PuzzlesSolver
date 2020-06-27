3d724ee2cf0c16903dd7af7edf4c9bc7c9de48c4
ThePPK <thepolishppk@op.pl>
2020-06-27 16:21:11 +0200

    Signpost got test for method: 	- getAllBlocksOnWay 	- getMapOfBlocksNotLinking 	- getMapOfBlocksNotLinked for Solve class

--- Games/Signpost/test.py
+++ Games/Signpost/test.py
@@ -1,23 +1,11 @@
-from _solve import Board, Block
+from _solve import Board, Block, Solve
+from _test_maps import *
 import unittest, pdb
 
-Board1_4x4 = (
-	( (1, Block.DIRECTION_BOTTOM_RIGHT), (None, Block.DIRECTION_BOTTOM), (None, Block.DIRECTION_BOTTOM_LEFT), (None, Block.DIRECTION_BOTTOM) ),
-	( (None, Block.DIRECTION_BOTTOM_RIGHT), (None, Block.DIRECTION_LEFT), (None, Block.DIRECTION_TOP_RIGHT), (None, Block.DIRECTION_TOP_LEFT) ),
-	( (None, Block.DIRECTION_BOTTOM), (None, Block.DIRECTION_TOP), (None, Block.DIRECTION_RIGHT), (None, Block.DIRECTION_TOP_LEFT) ),
-	( (None, Block.DIRECTION_TOP_RIGHT), (None, Block.DIRECTION_TOP_RIGHT), (None, Block.DIRECTION_TOP), (16, None) )
-)
-
-Solution1_4x4 = (
-	(1, 4, 7, 15),
-	(12, 11, 14, 6),
-	(8, 10, 2, 3),
-	(9, 5, 13, 16)
-)
 
 
 class Test_Board(unittest.TestCase):
-	def test_parsingBoardFromMatrix(self):
+	def Test_parsingBoardFromMatrix(self):
 		board = Board._parseMatrix(Board1_4x4)
 
 		for y in range(len(Board1_4x4)):
@@ -45,8 +33,95 @@ class Test_Board(unittest.TestCase):
 					)
 				)
 
+class Test_SolveMethods(unittest.TestCase):
+	def getMapOfBlocksNotLinking(self, solution, ways: list = []):
+		solver = Solve(Board.parse(TestLinks1))
+		solver.Ways = ways
 
+		linkingMap = solver.getMapOfBlocksNotLinking()
+		for y in range(len(linkingMap)):
+			for x in range(len(linkingMap[0])):
+				self.assertEqual(
+					linkingMap[y][x],
+					solution[y][x],
+					"Block on x:{} and y:{} is {}".format(
+						x,
+						y,
+						"not linking" if solution[y][x] else "linking"
+					)
+				)
 
+	def test_getMapOfBlocksNotLinking_noWays(self):
+		self.getMapOfBlocksNotLinking(TestLinks1_NotLinking)
+
+	def test_getMapOfBlocksNotLinking_withWays(self):
+		ways = [
+			[(2, 0), (2, 1), (1, 2)],
+			[(2, 3), (3, 2)]
+		]
+		solution = [ list(x) for x in list(TestLinks1_NotLinking).copy() ]
+		for x, y in ((2, 0), (2, 1), (2, 3)):
+			solution[y][x] = False
+
+		self.getMapOfBlocksNotLinking(solution, ways)
+
+	def getMapOfBlocksNotLinked(self, solution, ways: list = []):
+		solver = Solve(Board.parse(TestLinks1))
+		solver.Ways = ways
+
+		linkingMap = solver.getMapOfBlocksNotLinked()
+		for y in range(len(linkingMap)):
+			for x in range(len(linkingMap[0])):
+				self.assertEqual(
+					linkingMap[y][x],
+					solution[y][x],
+					"Block on x:{} and y:{} is {}".format(
+						x,
+						y,
+						"not linked" if solution[y][x] else "linked"
+					)
+				)
+
+	def test_getMapOfBlocksNotLinked_withWays(self):
+		ways = [
+			[(2, 0), (2, 1), (1, 2)],
+			[(2, 3), (3, 2)]
+		]
+		solution = [ list(x) for x in list(TestLinks1_NotLinked).copy() ]
+		for x, y in ((2, 1), (1, 2), (3, 2)):
+			solution[y][x] = False
+		self.getMapOfBlocksNotLinked(solution, ways)
+
+	def test_getMapOfBlocksNotLinked_noWays(self):
+		self.getMapOfBlocksNotLinked(TestLinks1_NotLinked)
+
+	def test_getAllBlocksOnWay(self):
+		board = Board.parse(TestLinks1)
+		solver = Solve(board)
+
+		dataSet = (
+			({board[2, 1], board[2, 0]}, (2, 2), "TOP"),
+			({board[2, 3]}, (2, 2), "BOTTOM"),
+			({board[2, 1], board[3, 1]}, (1, 1), "LEFT"),
+			({board[0, 1]}, (1, 1), "RIGHT"),
+			({board[2, 2], board[3, 1]}, (1, 3), "TOP_LEFT"),
+			({board[1, 1], board[2, 2], board[3, 3]}, (0, 0), "BOTTOM_LEFT"),
+			({board[1, 2], board[0, 3]}, (2, 1), "BOTTOM_RIGHT"),
+			({board[1, 2], board[0, 1]}, (2, 3), "TOP_RIGHT")
+		)
+
+		for data in dataSet:
+			self.assertEqual(
+				set(solver.getAllBlocksOnWay(
+					getattr(Block, "DIRECTION_"+data[2]),
+					data[1]
+				)),
+				data[0],
+				"Test getAllBlocksOnWay with way: {} and starting point: x={}, y={}".format(
+					" and ".join(x[0].upper()+x[1:].lower() for x in str(data[2]).split("_")),
+					*data[1]
+				)
+			)
 
 
 
